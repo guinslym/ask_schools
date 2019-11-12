162c0e3ee9f979a4ed0e09f1549020b19ac54c50
Guinsly Mondesir <guinslym@gmail.com>
2019-11-11 22:38:57 -0500

    adding tests

--- tests/test_ask_schools.py
+++ tests/test_ask_schools.py
@@ -1,5 +1,10 @@
 from ask_schools import __version__
+from ask_schools import find_schools_by_mentee_suffix
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def check_school_name_equal_toronto():
+    result = find_schools_by_mentee_suffix('_tor')
+    assert result == 'Tordonto'
